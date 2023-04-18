619355e49cd241d7a4a1287bab16e9d854499c49
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-04-18 10:21:51 +0800

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,31 @@
+def count_kmers(sequence, k_size):
+    data = {}
+    size = len(sequence)
+    for i in range(size - k_size + 1):
+        kmer = sequence[i: i + k_size]
+        try:
+            data[kmer] += 1
+        except KeyError:
+            data[kmer] = 1
+    return data
+
+output = count_kmers("CTGACTGACTGACTGTA", 3)
+
+print(output)
+
+#Generate the frequency of the coverage values
+
+def calculate_coverage_frequence(kmer_dataset):
+    _coverage_freq = {}
+    for coverage in kmer_dataset.values():
+        try:
+            _coverage_freq[coverage] += 1
+        except KeyError:
+            _coverage_freq[coverage] = 1
+    return _coverage_freq
+
+results = calculate_coverage_frequence(
+    {'CTG': 4, 'TGA': 3, 'GAC': 3, 'ACT': 3, 'TGT': 1, 'GTA': 1}
+)
+#dictionary that count how many times k-mers with coverage X appears in our sample:
+print(results)
